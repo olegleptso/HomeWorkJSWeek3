e2db4be60f9718210dbb1e53bd2afa480e8d0d12
Oleg Leptso <2nx23@ukr.net>
2018-10-28 18:28:47 +0200

    rework

--- 1.js
+++ 1.js
@@ -17,13 +17,20 @@ function minmax (array) {
 	var max = Math.max(...array);
 	console.log("Min: " + min);
 	console.log("Max: " + max);
+	return {
+		Min: min,
+		Max: max
+	}
 }
 
 function average (array) {
+	if (array.length == 0)
+		return 0;
 	var sum =0;
 	for (var i =0; i<array.length;i++) {
 		sum += array[i];
 	}
 	return sum/array.length;
 }
-
+var amx = [3,5,1,-2,3];
+minmax(amx);
